52fffee0ff934ada2b92af58e16cee929d1f89c4
theo <theo_portela@hotmail.com>
2024-04-18 18:22:31 -0300

    Update Project/create UseCases

--- app/src/main/kotlin/me/dio/copa/catar/MainActivity.kt
+++ app/src/main/kotlin/me/dio/copa/catar/MainActivity.kt
@@ -3,14 +3,26 @@ package me.dio.copa.catar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.dio.copa.catar.ui.theme.Copa2022Theme
+import me.dio.copa.catar.ui.theme.mainFunctionalitiesButtons
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +50,43 @@ fun Greeting(name: String) {
 @Composable
 @Preview(showBackground = true)
 fun DefaultPreview() {
-    Copa2022Theme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row {
+            Button(
+                onClick = ::GetMatchs,
+                colors = ButtonDefaults.buttonColors(backgroundColor = mainFunctionalitiesButtons)
+            ) {
+                Text(text = "coisa coisosa")
+            }
+        }
     }
+}
+
+@Composable
+fun BtnFunctionality(func: Functionality) {
+    Row {
+        Icon(
+            painter = painterResource(id = func.IconID),
+            contentDescription = null
+        )
+
+        Button(onClick = func.Url) {
+            Text(text = func.name)
+        }
+    }
+}
+
+data class Functionality(
+    var name: String,
+    var enabled: Boolean,
+    var IconID: Int,
+    var Url: () -> Unit
+)
+
+fun GetMatchs() {
+
 }
